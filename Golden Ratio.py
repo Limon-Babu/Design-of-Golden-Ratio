e2cd4cb0f237c330e7de3d9f7c0a6e3ee947b897
Limon-Babu <limonanobm5849@gmail.com>
2021-05-05 17:25:39 +0600

    Add files via upload

---
+++ Golden Ratio.py
@@ -0,0 +1,42 @@
+import turtle
+for i in range(2):
+	turtle.forward(210)
+	turtle.left(90)
+	turtle.forward(130)
+	turtle.left(90)
+	i+=1
+for i in range(3):
+	turtle.forward(50)
+	turtle.left(90)
+	i+=1
+turtle.left(90)
+for i in range(5):
+	turtle.forward(80)
+	turtle.left(90)
+	i+=1
+turtle.backward(50)
+for i in range(2):
+	turtle.forward(30)
+	turtle.left(90)
+	i+=1
+turtle.backward(10)
+turtle.left(90)
+turtle.forward(10)
+turtle.left(90)
+turtle.forward(10)
+turtle.backward(20)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(130)
+turtle.left(90)
+turtle.circle(130,90)
+turtle.circle(80,90)
+turtle.circle(50,90)
+turtle.circle(30,90)
+turtle.circle(20,90)
+turtle.circle(10,90)
+turtle.circle(10,90)
+turtle.exitonclick()
